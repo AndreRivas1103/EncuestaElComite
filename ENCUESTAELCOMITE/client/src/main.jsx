659e805304af6f7aedea399b668eb74a4d5b8e88
AndreRivas1103 <andrerivasgarcia85@gmail.com>
2025-04-15 22:25:38 -0500

    se añadio y se modificaron archivos para configurar el react

--- ENCUESTAELCOMITE/client/src/main.jsx
+++ ENCUESTAELCOMITE/client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// client/src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Página principal */}
+        <Route path="/" element={<Home />} />
+
+        {/* Otra ruta por si App.jsx es otra vista */}
+        <Route path="/encuesta" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
