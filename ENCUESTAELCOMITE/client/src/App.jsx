0b33f95c9e18159e29e4be54de697a78ebf8201d
AndreRivas1103 <andrerivasgarcia85@gmail.com>
2025-04-17 15:46:40 -0500

    se añadio react-helmet

--- ENCUESTAELCOMITE/client/src/App.jsx
+++ ENCUESTAELCOMITE/client/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/iniciar-sesion" element={<IniciarSesion />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   )
