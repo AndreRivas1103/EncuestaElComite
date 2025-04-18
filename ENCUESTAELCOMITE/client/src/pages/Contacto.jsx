c4e88ff5c74ac02513649d852043f2b26a487473
AndreRivas1103 <andrerivasgarcia85@gmail.com>
2025-04-17 22:07:23 -0500

    pantalla nuevo de contacto.jsx

---
+++ ENCUESTAELCOMITE/client/src/pages/Contacto.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../pages/styles/IniciarSesion.css';
+import babyLogo from '../assets/gobabygo.png';
+import { Link } from 'react-router-dom';
+
+
+const IniciarSesion = () => {
+  return (
+    <div className="login-container">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Gloock&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Recoleta&display=swap" rel="stylesheet" />
+      
+      {/* Header con logo y título */}
+      <header className="header">
+        <div className="logo">
+          <a href='/'>El Comit<span>é</span></a>
+        </div>
+        <img src={babyLogo} alt="Baby Go Logo" className="header-logo" />
+      </header>
+      <h1>Aun no hay una monda jaja</h1>
+      <h1>Asi que parchese</h1>
+      <h1>Contacto Proximamente :D</h1>
+    </div>
+  );
+};
+
+export default IniciarSesion;
