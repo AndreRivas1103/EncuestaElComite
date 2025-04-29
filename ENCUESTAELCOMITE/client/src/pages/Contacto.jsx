08e7f1368066194436459b786691681a08fbda15
AndreRivas1103 <andrerivasgarcia85@gmail.com>
2025-04-29 00:12:28 -0500

    Cambios en el contenido de la pagina de contacto y su logo

--- ENCUESTAELCOMITE/client/src/pages/Contacto.jsx
+++ ENCUESTAELCOMITE/client/src/pages/Contacto.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../Pages/styles/Home.css';
-import babyLogo from '../assets/logogdc.jpg';
+import babyLogo from '../assets/LogoMarcaPersonal.png';
 import { Link } from 'react-router-dom';
 
 
 const IniciarSesion = () => {
   return (
     <div className="login-container">
+      <title>Contacto</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css2?family=Gloock&display=swap" rel="stylesheet" />
@@ -19,9 +20,12 @@ const IniciarSesion = () => {
         </div>
         <img src={babyLogo} alt="Baby Go Logo" className="header-logo" />
       </header>
-      <h1>Contacto</h1>
-      <h1>Telefono</h1>
-      <h1>Correo Electronico: </h1>
+      <h1 className='title-contacto-1'>Contacto</h1>
+      <p className='text-contacto'></p>
+      <h1 className='title-contacto'>Telefono</h1>
+      <p className='text-contacto'>+57 (604) 320 2160</p>
+      <h1 className='title-contacto'>Correo Electronico</h1>
+      <p className='text-contacto'>Informacion@elcomite.org.co</p>
     </div>
   );
 };
