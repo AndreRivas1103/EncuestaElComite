86a2c3de73f6b0a4456ef6a44bdfaa0176123793
Samuel Gallego <samuelgallego441@gmail.com>
2026-04-19 15:34:23 -0500

    all connected

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App'
 import './index.css'
+
+const apiOrigin = import.meta.env.VITE_API_ORIGIN || 'http://localhost:3010'
+
+axios.interceptors.request.use((config) => {
+  if (typeof config.url === 'string' && config.url.startsWith('http://localhost:3000')) {
+    config.url = config.url.replace('http://localhost:3000', apiOrigin)
+  }
+  return config
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- packages/server/server.js
+++ packages/server/server.js
@@ -18,10 +18,14 @@ const openapiDocument = JSON.parse(
 );
 const app = express();
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000'
+];
 
 // Middlewares esenciales
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
